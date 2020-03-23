d17d023ab8e57ef3cf8120a5214e489f3a718071
Donghoon Lee <movehoon@gmail.com>
2020-03-23 21:01:32 +0900

    [qnode/node]Send titan command

--- Arduino/qnode/qnode.ino
+++ Arduino/qnode/qnode.ino
@@ -7,6 +7,11 @@
 
 #define LED_BUILTIN 2
 #define PIN_MODE 32
+#define PIN_485EN 4
+
+#define PIN_LED1  25
+#define PIN_LED2  26
+#define PIN_LED3  27
 
 enum {
   LED_NONE,
@@ -26,6 +31,9 @@ LedIndicator ledIndicator;
 bool mode_wifi_f = false;
 
 bool req_ble_report_f;
+bool req_titan_monitor_f;
+
+int test_led_count;
 
 int count;
 char buff[256];
@@ -49,7 +57,13 @@ void IRAM_ATTR onTimer(){
     RequestWifiProcess();
   }
 
+  if (timer_count_100ms % 10) {
+    test_led_count++;
+  }
+
   req_ble_report_f = true;
+
+  req_titan_monitor_f = true;
 
   portEXIT_CRITICAL_ISR(&timerMux);
   // Give a semaphore that we can check in the loop
@@ -68,9 +82,15 @@ void setup()
   
   // led turned on/off from the iPhone app
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_485EN, OUTPUT);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  pinMode(PIN_LED3, OUTPUT);
   
   // button press will be shown on the iPhone app)
   pinMode(button, INPUT);
+  pinMode(PIN_MODE, INPUT);
+  mode_wifi_f = digitalRead(PIN_MODE);
 
   LoadPreference();
   printf("ap=%s\n", GetPreferenceAP());
@@ -102,6 +122,36 @@ void setup()
 void loop()
 {
   delay(1);
+
+//  switch(test_led_count%3) {
+//    case 0:
+//      digitalWrite(PIN_LED1, 1);
+//      digitalWrite(PIN_LED2, 0);
+//      digitalWrite(PIN_LED3, 0);
+//      break;
+//    case 1:
+//      digitalWrite(PIN_LED1, 0);
+//      digitalWrite(PIN_LED2, 1);u
+//      digitalWrite(PIN_LED3, 0);
+//      break;
+//    case 2:
+//      digitalWrite(PIN_LED1, 0);
+//      digitalWrite(PIN_LED2, 0);
+//      digitalWrite(PIN_LED3, 1);
+//      break;
+//  }
+
+//  printf("Mode: %d\n", digitalRead(PIN_MODE));
+
+  if (req_titan_monitor_f) {
+    req_titan_monitor_f = false;
+
+    // Request command to Titan
+    digitalWrite(PIN_485EN, HIGH);
+    Serial2.write("\n@01:CURQA\n");
+    Serial2.flush();
+    digitalWrite(PIN_485EN, LOW);
+  }
 
   if (mode_wifi_f) {
     loopWifi();
@@ -164,7 +214,7 @@ void serialEvent () {
     if (recv_cmd[cmd_index] == 0x0A || recv_cmd[cmd_index] == 0x0D) {
       recv_cmd[cmd_index] = 0x00;
 
-      Serial.printf("[U]%s\n", recv_cmd);
+      Serial.printf("[U1]%s\n", recv_cmd);
       parse((char *)recv_cmd);
 
       cmd_index = 0;
@@ -178,7 +228,9 @@ void serialEvent () {
     if (recv_cmd2[cmd2_index] == 0x0A) {
       recv_cmd2[cmd2_index+1] = 0x00;
 
-      SendSplit(recv_cmd2, cmd2_index+1, 20);
+      if (BleConnected()) {
+        SendSplit(recv_cmd2, cmd2_index+1, 20);
+      }
 //      uint8_t sendCount = cmd2_index / 20 + 1;
 //      for (uint8_t i=0; i<sendCount; i++) {
 //        if (i < sendCount-1)
@@ -186,7 +238,7 @@ void serialEvent () {
 //        else
 //          BleSend(recv_cmd2+(i*20), cmd2_index%20);
 //      }
-      Serial.printf("[U]%s\n", recv_cmd2);
+      Serial.printf("[U2]%s\n", recv_cmd2);
 
       cmd2_index = 0;
       continue;
