6a12a4fc90fd262d5d1d5d61800280499ac16368
Donghoon Lee <movehoon@gmail.com>
2020-11-08 21:17:14 +0900

    [qnode_ws] Change WebSocket library and code by Chris

--- Arduino/qnode_ws/preference.ino
+++ Arduino/qnode_ws/preference.ino
@@ -1,138 +1,23 @@
 #include <Preferences.h>
 
 #define PREFS_NAME "saved_data"
-#define PREFS_MODE "MD"
-#define PREFS_AP "AP"
-#define PREFS_AP_1 "AP_1"
-#define PREFS_PW "PW"
-#define PREFS_PW_1 "PW_1"
-#define PREFS_IP "IP"
-#define PREFS_IP_1 "IP_1"
 #define PREFS_ID  "ID"
-#define PREFS_ID_1 "ID_1"
 
-#define PREFS_DEFAULT_MODE "BT"
-#define PREFS_DEFAULT_AP ""
-#define PREFS_DEFAULT_PW ""
-#define PREFS_DEFAULT_IP "121.137.95.17"
-#define PREFS_DEFAULT_ID ""
+#define PREFS_DEFAULT_ID "02"
 
 Preferences prefs;
 
-String md;
-String ap;
-String pw;
-String ip;
 String id;
-
 
 void LoadPreference() {
   prefs.begin(PREFS_NAME);
-//  md = prefs.getString(PREFS_MODE, PREFS_DEFAULT_md);
-//  ap = prefs.getString(PREFS_AP, PREFS_DEFAULT_AP);
-//  pw = prefs.getString(PREFS_PW, PREFS_DEFAULT_PW);
-//  ip = prefs.getString(PREFS_IP, PREFS_DEFAULT_IP);
-}
-
-const char *GetPreferenceMode() {
-  md = prefs.getString(PREFS_MODE, PREFS_DEFAULT_MODE);
-  return md.c_str();
-}
-
-const char *GetPreferenceAP() {
-  ap = prefs.getString(PREFS_AP, PREFS_DEFAULT_AP);
-  if (ap.length() >= 8) {
-    ap += prefs.getString(PREFS_AP_1, "");
-  }
-  return ap.c_str();
-}
-
-const char *GetPreferencePW() {
-  pw = prefs.getString(PREFS_PW, PREFS_DEFAULT_PW);
-  if (pw.length() >= 8) {
-    pw += prefs.getString(PREFS_PW_1, "");
-  }
-  return pw.c_str();
-}
-
-const char *GetPreferenceIP() {
-  ip = prefs.getString(PREFS_IP, PREFS_DEFAULT_IP);
-  if (ip.length() >= 8) {
-    ip += prefs.getString(PREFS_IP_1, "");
-  }
-  return ip.c_str();
 }
 
 const char *GetPreferenceID() {
-  id = prefs.getString(PREFS_ID, PREFS_DEFAULT_ID);
-  if (id.length() >= 8) {
-    id += prefs.getString(PREFS_ID_1, "");
-  }
+  id = prefs.getString(PREFS_ID,PREFS_DEFAULT_ID);
   return id.c_str();
 }
 
-//String GetPreference(String key) {
-//  if (key == PREFS_AP) {
-//    ap = prefs.getString(PREFS_AP, PREFS_DEFAULT_AP).c_str();
-//    return ap.c_str();
-//  }
-//  else if (key == PREFS_PW) {
-//    pw = prefs.getString(PREFS_PW, PREFS_DEFAULT_PW).c_str();
-//    return String(pw.c_str());
-//  }
-//  else if (key == PREFS_IP) {
-//    ip = prefs.getString(PREFS_IP, PREFS_DEFAULT_IP).c_str();
-//    printf("ip:%s\n", ip);
-//    return String(ip.c_str());
-//  }
-//  return "";
-//}
-
-void SavePreferenceMode(String value) {
-  prefs.putString(PREFS_MODE, value);
-}
-void SavePreferenceAP(String value) {
-  prefs.putString(PREFS_AP, value.substring(0, 8));
-  if (value.length() > 8) {
-    prefs.putString(PREFS_AP_1, value.substring(8, 16));
-  }
-  else {
-    prefs.putString(PREFS_AP_1, "");
-  }
-}
-void SavePreferencePW(String value) {
-  prefs.putString(PREFS_PW, value.substring(0, 8));
-  if (value.length() > 8) {
-    prefs.putString(PREFS_PW_1, value.substring(8, 16));
-  }
-  else {
-    prefs.putString(PREFS_PW_1, "");
-  }
-}
-void SavePreferenceIP(String value) {
-  prefs.putString(PREFS_IP, value.substring(0, 8));
-  if (value.length() > 8) {
-    prefs.putString(PREFS_IP_1, value.substring(8, 16));
-  }
-  else {
-    prefs.putString(PREFS_IP_1, "");
-  }
-}
 void SavePreferenceID(String value) {
-  prefs.putString(PREFS_ID, value.substring(0, 8));
-  if (value.length() > 8) {
-    prefs.putString(PREFS_ID_1, value.substring(8, 16));
-  }
-  else {
-    prefs.putString(PREFS_ID_1, "");
-  }
+  prefs.putString(PREFS_ID, value);
 }
-
-//void SavePreference(String key, String value) {
-//  if (key == PREFS_AP)
-//    prefs.putString(PREFS_AP, value);
-//  else if (key == PREFS_PW)
-//    prefs.putString(PREFS_PW, value);
-//  else if (key == PREFS_IP)
-//    prefs.putString(PREFS_IP, value);
-//}
--- Arduino/qnode_ws/qnode_ws.ino
+++ Arduino/qnode_ws/qnode_ws.ino
@@ -1,391 +1,310 @@
-#include "ESPAsyncWebServer.h"
+#include <WiFi.h>
+#include <WebSocketsServer.h>
 
-#define MODEL_TITAN
-//#define MODEL_QNODE
+#define LED_BUILTIN 13
 
-#ifdef MODEL_TITAN
-#define ENABLE_WS
-#endif
+char ssid[32];
+char password[32];
+int counter = 0;
+char str[256],push_cmd_str[128],cmd_str[128],reply_str[256],serial_recv_str[128];
+int g_tid=1,tid=0,ret;
+long templ=1000;
+int cmd_index=0;
+int debug_mode=0;
+int push_data=0;
+int connect_stat;
+char remoteIPAddress[32];
+IPAddress myip;
 
-//#define ENABLE_SDCARD
-#define ENABLE_DEBUGGING
+int TITANCommandReply(char command[], char reply[]);
 
-#define PREFS_MODE "MODE"
-#define PREFS_AP "AP"
-#define PREFS_PW "PW"
-#define PREFS_IP "IP"
+// Globals
+WebSocketsServer webSocket = WebSocketsServer(80);
 
-#define LED_BUILTIN 25
-#define PIN_MODE 32
-#define PIN_485EN 4
-
-#define PIN_LED_B  13
-#define PIN_LED_G  26
-#define PIN_LED_R  27
-#define LED_R 0
-#define LED_G 1
-#define LED_B 2
-
-#define TITAN_ID_MAX 100
-
-//@01:SVON
-//#01:SVON=1
-//@01:SVOFF
-//#01:SVOFF=1
-//@01:SVON
-//#01:SVON=1
-//@01:HSPD=350
-//#01:HSPD=350
-//@01:ACC=5000
-//#01:ACC=5000
-//@01:X=12345
-//#01:X=1
-//@01:X=-54321
-//#01:X=1
-//@01:JOGXP
-//#01:JOGXP=1
-//@01:STOPX
-//#01:STOPX=1
-//@01:JOGXN
-//#01:JOGXN=1
-//@01:STOPX
-//#01:STOPX=1
-
-const char* ssid = "KT_GiGA_2G_Wave2_F763";
-const char* password =  "0ahebzh872";
-
-AsyncWebServer server(80);
-AsyncWebSocket ws("/titan");
-AsyncWebSocketClient *wsClient = 0;
-char ws_msg[256];
-
-
-const int LED_FREQ = 5000;
-
-int mode;
-int lcd_init_count = 30;
-bool system_ready = false;
-
-bool req_titan_monitor_f;
-
-bool recv_packet_f;
-int recv_packet_count;
-
-char WID[32] = "";
-
-void LogD(const char *message) {
-#ifdef ENABLE_DEBUGGING
-  Serial.print(message);
-#endif
-}
-void LogDln(const char *message) {
-#ifdef ENABLE_DEBUGGING
-  Serial.println(message);
-#endif
-}
-
-int count;
-char buff[256];
-char titan2_buff[256];
-
-int wifi_reset_count = 0;
-
-int found_titanID = 0;
-void SearchTitanID() {
-//  Serial.println("SearchTitanID");
-  delay(100);
-  for (uint8_t i=0; i<TITAN_ID_MAX; i++) {
-    sprintf(titan2_buff, "@%02d:FIRMVS\n", i);
-    Serial2.print(titan2_buff);
-    delay(10);
-    serialEvent();
-    if (found_titanID > 0) {
-      break;
-    }
-  }
-}
-
-void GetTitanWID() {
-  delay(100);
-//  Serial.println("GetTitanWID");
-  sprintf(titan2_buff, "@%02d:WID\n", found_titanID);
-//  Serial.print(titan2_buff);
-  Serial2.print(titan2_buff);
-  delay(5);
-  serialEvent();
-}
-
-void onWsEvent( AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len ) {
-  wsClient = client;
-  if(type == WS_EVT_CONNECT) {
-    LogDln("Websocket client connection received");
-  }
-  else if(type == WS_EVT_DISCONNECT) {
-    wsClient = 0;
-    LogDln("Client disconnected");
-  }
-  else if(type == WS_EVT_DATA) {
-//    Serial.println("Data received: ");
-//    for(int i=0; i < len; i++) {
-//      Serial.print(data[i]);
-//      Serial.print("|");
-//    }
-//    Serial.println();
-
-    memset(titan2_buff, 0, sizeof(titan2_buff));
-    if (len > sizeof(titan2_buff))
-      len = sizeof(titan2_buff);
-    memcpy(titan2_buff, data, len);
-    Send2Titan(titan2_buff);
-    
-//    count++;
-//    sprintf(ws_msg, "Reply %d", count);
-//    client->text(ws_msg);
-  }
-}
-
-void setupWS(){
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    
-    Serial.println("Connecting to WiFi..");
-
-    wifi_reset_count++;
-    if (wifi_reset_count > 10)
-      ESP.restart();
-
-    serialEvent();
-  }
-
-  Serial.println(WiFi.localIP());
-
-  ws.onEvent(onWsEvent);
-  server.addHandler(&ws);
-
-  server.begin();
-}
-
-char tmp_msg[256];
-void setup()
+//************************************ Called back for when receiving any WebSocket message
+void onWebSocketEvent(uint8_t num,  
+                      WStype_t type, 
+                      uint8_t * payload, 
+                      size_t length) 
 {
-  delay(3000);
-
-  Serial.begin(115200);
-  Serial2.begin(115200);
-
-  printf("\nStart\n");
-  
-  // led turned on/off from the iPhone app
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(PIN_485EN, OUTPUT);
-
-  LoadPreference();
-  printf("AP=%s\n", GetPreferenceAP());
-  printf("PW=%s\n", GetPreferencePW());
-  printf("IP=%s\n", GetPreferenceIP());
-  printf("ID=%s\n", GetPreferenceID());
-
-  SearchTitanID();
-  GetTitanWID();
-
-#ifdef ENABLE_SDCARD
-  setupSdcard();
-#endif
-
-  setupWS();
-
-  system_ready = true;
-}
-
-void Send2Titan(const char *msg) {
-#ifdef ENABLE_DEBUGGING
-  printf("Send2Titan %s\n", msg);
-#endif
-  digitalWrite(PIN_485EN, HIGH);
-  Serial2.println(msg);
-  Serial2.flush();
-  digitalWrite(PIN_485EN, LOW);
-}
-
-void loop()
-{
-  delay(1);
-
-  serialEvent();
-}
-
-uint8_t recv_cmd[256+1];
-uint8_t cmd_index;
-char recv_cmd2[256+1];
-uint8_t cmd2_index;
-bool u1_data_catch;
-uint8_t u1_cmd[256+1];
-uint8_t u1_resp_message[32];
-void serialEvent () {
-  while(Serial.available()) {
-    recv_cmd[cmd_index] = (uint8_t)Serial.read();
-    if (recv_cmd[cmd_index] == 0x0A || recv_cmd[cmd_index] == 0x0D) {
-      recv_cmd[cmd_index] = 0x00;
-      memcpy(u1_cmd, recv_cmd, cmd_index+1);
-
-      char *p_token;
-      char buf[32];
-      char value[32];
-
-      LogD("[U1]");
-      LogD((const char *)recv_cmd);
-      LogD("\n");
-      u1_data_catch = false;
-
-      if (recv_cmd[0] == '@') {
-        if (!u1_data_catch) {
-          Send2Titan((const char *)u1_cmd);
+        switch(type) {// Figure out the type of WebSocket event
+                case WStype_DISCONNECTED: // Client has disconnected
+                        if (debug_mode>0) Serial.printf("[%u] Disconnected!\n", num);
+                        connect_stat=0;
+                        break;
+                case WStype_CONNECTED: // New client has connected
+                {
+                        IPAddress ip = webSocket.remoteIP(num);
+                        connect_stat=1;
+                        sprintf(remoteIPAddress,"%d.%d.%d.%d",ip[0],ip[1],ip[2],ip[3]);
+                        if (debug_mode>0) {
+                          Serial.printf("[%u] Connection from %s\n", num, remoteIPAddress);
+                        }
+                        break;
+                }
+                case WStype_TEXT: // Service text message from the client
+                        sprintf(cmd_str,"%s",payload);
+                        if (debug_mode>0) Serial.printf("[%u] Send:(%d) %s\n", num, strlen(cmd_str),cmd_str);
+                        ret = TITANCommandReply(cmd_str,reply_str);
+                        //sprintf(reply_str,"#01:EX=%ld;VX=506.85;POSD=2997243;VPROF=500;MST=0x5;CURQA=0.0102675;CURQD=0.0558196;CURDD=-0.158617;DIN=0x0;DOUT=0x0;LED=1;RGB=4",++templ);
+                        if (debug_mode>0) Serial.printf("[%u] Reply:(%d) %s\n", num, strlen(reply_str),reply_str);
+                        webSocket.sendTXT(num, reply_str);
+                        break;
+                case WStype_BIN: // For everything else: do nothing
+                case WStype_ERROR:
+                case WStype_FRAGMENT_TEXT_START:
+                case WStype_FRAGMENT_BIN_START:
+                case WStype_FRAGMENT:
+                case WStype_FRAGMENT_FIN:
+                default:
+                        break;
         }
-      }
+}
 
-      if (!u1_data_catch) {
-        parse((char *)u1_cmd);
+//************************ Serial Flush Input Buffers
+void serial_flush(void) {
+       while (Serial2.available()) { Serial.printf("(Flush:%c)\n",Serial2.read());}
+}
+
+//************************ TITAN Command and Reply 
+
+int TITANCommandReply(char command[], char reply[])
+{
+      int ri=0,toi=0;
+      char cc;
+      reply[0]='\0';
+      serial_flush();
+      Serial2.printf("%s\n",command);
+      while (true){
+          if (Serial2.available()){       
+            cc = Serial2.read();
+            if (cc=='^'){
+                ESP.restart();
+            }
+            if ((cc != '\n')&&(cc != '\r')) {
+                reply[ri++]=cc;
+                reply[ri]='\0';
+            }
+            if (ri>255) return ri;
+            if ((cc == '\n')) {
+              serial_flush();
+              return ri;
+            }
+            toi=0;
+          }
+          else{
+            delay(1);
+            if (++toi>1000) return -1;
+          }
       }
+}
+
+//************************** Check Reset Command
+void checkReset()
+{
+      char cc;
+      if (Serial2.available()){       
+         cc = Serial2.read();
+         if (cc=='^'){
+            ESP.restart();   
+         }
+      }
+}
+
+//************************** Setup
+void setup() {
+        int i,ipa1,ipa2,ipa3,ipa4, ret,bit=0;
+        long ipalong;
+        char str[64],replystr[64],cmdstr[64];
       
-      cmd_index = 0;
-      continue;
-    }
-    cmd_index++;
-  }
-  
-  while(Serial2.available()) {
-    recv_cmd2[cmd2_index] = (uint8_t)Serial2.read();
-    if (recv_cmd2[cmd2_index] == 0x0A) {
-      Serial.printf("[U2]%s", recv_cmd2);
-      recv_cmd2[cmd2_index+1] = 0x00;
-#ifdef ENABLE_SDCARD
-      WriteMessage((char *)recv_cmd2);
-#endif
-
-#ifdef ENABLE_WS
-      if (wsClient) {
-        wsClient->text((const char *)recv_cmd2, strlen(recv_cmd2));
-//        wsClient->close();
-      }
-#endif
-
-      parseTitan((char *)recv_cmd2);
-
-      recv_packet_f = true;
-
-      memset(recv_cmd2, 0, sizeof(recv_cmd2));
-      cmd2_index = 0;
-      continue;
-    }
-    cmd2_index++;
-  }
-}
-
-void parseTitan(char* cmd) {
-  uint8_t start_index = 0;
-  char buf[32];
-  char value[32];
-//#01:EX=2;VX=0;MST=0x0;CURQA=-0.0263982;CURQD=0;CURDD=0;DIN=0x0;DOUT=0x0
-//   Serial.print("parsing: ");
-//   Serial.println(cmd);
-  
-  char *p_token;
-  char *c_token;
-  p_token = strtok(cmd, ":");
-
-  if (p_token) {
-//    Serial.println("foundID");
-//    Serial.println(p_token);
-    found_titanID = atoi(p_token+1);
-//    Serial.print("Found id ");
-//    Serial.println(found_titanID);
-    
-    if (strncmp(p_token, "#", 1) == 0) {
-      p_token = strtok(NULL, ":");
-//      Serial.println(p_token);
-
-      p_token = strtok(p_token, ";");
-      while (p_token) {
-        strcpy(buf, p_token);
-//        Serial.println(buf);
-
-        if (strncmp(buf, "WID=", 4) == 0) {
-          if (strlen(WID) > 0) {
-//            ESP.restart();
-          }
-          strcpy(WID, buf+4);
-          for (uint8_t i=0; i < sizeof(WID); i++) {
-            if (WID[i] == '\r' || WID[i] == '\n')
-              WID[i] = '\0';
-          }
-          Serial.print("Get WID ");
-          Serial.println(WID);
+        delay(1000);  //***Wait 1 seconds at startup
+        pinMode(LED_BUILTIN, OUTPUT);
+        for (i=0;i<20;i++){ //***Flash LED while waiting for TITAN to boot up
+            digitalWrite(LED_BUILTIN, 0);
+            delay(i*5);
+            digitalWrite(LED_BUILTIN, 1);
+            delay(i*5);
         }
-        p_token = strtok(NULL, ";");
-      }
-    }
-  }
+        digitalWrite(LED_BUILTIN, 1);
+      
+        // Start Serial port
+        Serial.begin(115200);
+        Serial2.begin(115200);
+        Serial2.flush();
+
+        //***Load Preferences stored in flash
+        LoadPreference();
+        sscanf(GetPreferenceID(), "%d", &g_tid);
+        if (g_tid<1 || g_tid>50) {
+          printf("Default Titan ID reset from %02d to 01\n",g_tid);
+          g_tid=1;
+        }
+        else{
+          printf("Main Titan ID %02d\n",g_tid);
+        }
+        sprintf(cmdstr,"@%02d:FIRMVS",g_tid);
+        i = TITANCommandReply(cmdstr,replystr);
+        if (i<1){
+              printf("No communication with main TITAN.  Check TITAN ID!\n");    
+              sprintf(ssid, "NA");
+              sprintf(password, "NA");
+        }
+        else{
+              sprintf(cmdstr,"@%02d:WID",g_tid); //*** Get WiFi SSID
+              i = TITANCommandReply(cmdstr,replystr);
+              sscanf(replystr,"#%d:WID=%s",&ret,ssid);
+              Serial.printf("SSID=%s\n",ssid);
+            
+              sprintf(cmdstr,"@%02d:WPW",g_tid); //*** Get WiFi Password
+              i = TITANCommandReply(cmdstr,replystr);
+              sscanf(replystr,"#%d:WPW=%s",&ret,password);
+              Serial.printf("PWD=%s\n",password);
+              
+              // Connect to WiFi access point
+              Serial.printf("Connecting %s:%s ",ssid,password);
+              WiFi.begin(ssid, password);
+              ret =1;
+              while (ret<10){ //*** Check connection to WiFi access point
+                if ( WiFi.status() == WL_CONNECTED ) {
+                    ret = 300; //***Connection success!
+                }
+                else{
+                    for (i=0;i<10;i++){
+                        digitalWrite(LED_BUILTIN, bit);
+                        if (bit) bit=0; else bit=1;
+                        delay(100);
+                        checkReset();
+                    }
+                    ret++;
+                    Serial.printf(".");
+                }
+              }
+              Serial.printf("\n");
+              digitalWrite(LED_BUILTIN, 1);
+              if (ret==300){
+                    myip = WiFi.localIP();
+
+                    Serial.printf("Connect Success and my IP = %d.%d.%d.%d\n",myip[0],myip[1],myip[2],myip[3]);
+                    ipalong = (long)(myip[0]) | (long)(myip[1])<<8 | (long)(myip[2])<<16 | (long)(myip[3])<<24;
+                    sprintf(cmdstr,"@%02d:WL_ADD=%ld",g_tid,ipalong);
+                    i = TITANCommandReply(cmdstr,replystr); //***Upload the IP address to the TITAN
+                    Serial.println("*** Ready and Websocket Server Started ***");
+                    webSocket.begin(); // Start WebSocket server and assign callback
+                    webSocket.onEvent(onWebSocketEvent);
+              }
+              else{
+                    Serial.print("Connection Unsuccesful!  Check SSID and PWD\n");
+              }
+        }
+        sprintf(push_cmd_str,"@%02d:EX;VX;POSD;VPROF;MST;CURQA;CURQD;CURDD;DIN;DOUT;LED;RGB",g_tid);
 }
 
-char msg[256];
+//****************************** Main Loop 
+void loop() {
+        // Look for and handle WebSocket data
+        webSocket.loop();
+        serialEvent();
+        checkReset();
+        if (push_data>0 && connect_stat==1){
+            ret = TITANCommandReply(push_cmd_str,reply_str);
+            sprintf(reply_str,"ALL:%s\n",reply_str); 
+            webSocket.sendTXT(0, reply_str);
+            delay(push_data);
+       }
+}
+//*********************** Serial Event Handler
+void serialEvent () {
+        while(Serial.available()) {
+          serial_recv_str[cmd_index] = (uint8_t)Serial.read();
+          if (serial_recv_str[cmd_index] == 0x0A || serial_recv_str[cmd_index] == 0x0D) {
+            serial_recv_str[cmd_index] = 0x00;
+            //memcpy(cmd_str, serial_recv_str, cmd_index+1);
+            if (serial_recv_str[0] == '@') {
+                ret = sscanf(serial_recv_str,"@%02d:",&tid);
+                if (ret==1 && tid == g_tid){
+                    ret = TITANCommandReply(serial_recv_str, reply_str);
+                    if (ret>0){
+                        Serial.printf("%s\n",reply_str);
+                        Serial.flush();
+                    }
+                }
+                //Send2Titan((const char *)u1_cmd);
+            }
+            else{
+              parse((char *)serial_recv_str);
+            }
+            cmd_index = 0;
+            while (Serial.available()) { 
+              Serial.read();
+            }
+            continue;
+          }
+          cmd_index++;
+        }
+}
+
+void substring(char s[], char sub[], int p, int l) {
+         int c = 0;
+         while (c < l) {
+            sub[c] = s[p+c];
+            c++;
+         }
+         sub[c] = '\0';
+}
+//***************************Parse
 void parse(char* cmd) {
-  LogD("parsing: ");
-  LogDln(cmd);
- 
-  if (strlen(cmd)==0)
-    return;
-  
-  char *p_token = (char *)strtok(cmd, "=");
-  
-  if (strncmp(p_token, "#", 1) == 0) {
-    // Ignore comment
-  }
-  else if (strncmp(p_token,"mo",2)==0) {
-    p_token = (char *)strtok(NULL, "=");
-    if (p_token) {
-      SavePreferenceMode(p_token);
-    }
-    sprintf(msg, "mo=%s\n", GetPreferenceMode());
-    printf(msg);
-//    SendSplit((uint8_t *)msg, strlen(msg), 20);
-  }
-  else if (strncmp(p_token,"ap",2)==0) {
-    p_token = (char *)strtok(NULL, "=");
-    if (p_token) {
-      SavePreferenceAP(p_token);
-    }
-    sprintf(msg, "ap=%s\n", GetPreferenceAP());
-    printf(msg);
-//    SendSplit((uint8_t *)msg, strlen(msg), 20);
-  }
-  else if (strncmp(p_token,"pw",2)==0) {
-    p_token = (char *)strtok(NULL, "=");
-    if (p_token) {
-      SavePreferencePW(p_token);
-    }
-    sprintf(msg, "pw=%s\n", GetPreferencePW());
-    printf(msg);
-//    SendSplit((uint8_t *)msg, strlen(msg), 20);
-  }
-  else if (strncmp(p_token,"ip",2)==0) {
-    p_token = (char *)strtok(NULL, "=");
-    if (p_token) {
-      SavePreferenceIP(p_token);
-      printf("Save ip to %s\n", p_token);
-    }
-    sprintf(msg, "ip=%s\n", GetPreferenceIP());
-    printf(msg);
-//    SendSplit((uint8_t *)msg, strlen(msg), 20);
-  }
-  else if (strncmp(p_token,"re",2)==0) {
-    printf("Restart Requested\n");
-    ESP.restart();
-  }
-  else {
-    LogD("Unknown Command");
-    LogDln(cmd);
-//    Serial.printf("Unknown Command %s\n", cmd);
-  }
+        char str1[16],str2[16],msg[32];
+        int id;
+        //Serial.printf("parsing: %s\n",cmd);
+        if (strlen(cmd)==0)
+          return;
+      
+        ret = sscanf(cmd,"%s",str1);
+        
+        if (strcmp(str1,"ID")==0) {
+          sprintf(msg, "ID=%s\n", GetPreferenceID());
+          printf(msg);
+        }
+        else if (str1[0]=='I' && str1[1]=='D' && str1[2]=='=') {
+            substring(str1, str2,3, strlen(str1)-3);
+            ret = sscanf(str2,"%d",&id);
+            if (id<1 || id>50) {
+              printf("ID set from %s to default 01\n",str2);
+              id=1;
+            }
+            sprintf(str2,"%02d",id);
+            SavePreferenceID(str2);
+            sprintf(msg, "ID=%s\n", GetPreferenceID());
+        }
+        else if (str1[0]=='D' && str1[1]=='B' && str1[2]=='=') {
+            substring(str1, str2,3, strlen(str1)-3);
+            ret = sscanf(str2,"%d",&id);
+            debug_mode=id;
+            sprintf(msg, "DB=%d\n", debug_mode);
+        }
+        else if (str1[0]=='P' && str1[1]=='D' && str1[2]=='=') {
+            substring(str1, str2,3, strlen(str1)-3);
+            ret = sscanf(str2,"%d",&id);
+            push_data=id;
+            sprintf(msg, "PD=%d\n", push_data);
+        }
+        else if (strcmp(str1,"SSID")==0) {
+          sprintf(msg, "SSID=%s\n", ssid);
+          printf(msg);
+        }
+        else if (strcmp(str1,"PWD")==0) {
+          sprintf(msg, "PWD=%s\n", password);
+          printf(msg);
+        }
+        else if (strcmp(str1,"IPA")==0) {
+          myip = WiFi.localIP();
+          Serial.printf("IPA=%d.%d.%d.%d\n",myip[0],myip[1],myip[2],myip[3]);
+        }
+        else if (strcmp(str1,"RESET")==0) {
+          printf("Resetting Wifi Module\n");
+          ESP.restart();
+        }
+        else{
+          printf("?%s\n",str1);
+        }
 }
